1f5b99ee56be7e97745c2f8bf2c622d25431c87a
Matthew Hoffman <mhoffman@lanl.gov>
2026-03-11 17:31:56 -0600

    Fix SHTns Fortran wrapper symbol/ISO-C binding conflicts
    Replace problematic direct include-based SHTns F03 usage with explicit minimal bind(C) interfaces in shtns_backend_mod, avoiding host-association name collisions and C_* kind resolution issues seen with some compilers/toolchains.
    
    Also ensure destroy/configure paths call the C bindings unambiguously (shtns_destroy_c, shtns_unset_grid_c, etc.) while keeping backend API unchanged for dispatcher integration.

--- shtns_backend_mod.f90
+++ shtns_backend_mod.f90
@@ -1,11 +1,16 @@
 module shtns_backend_mod
 
-   use iso_c_binding, only: c_int, c_double, c_double_complex, c_ptr, c_null_ptr, c_associated, c_f_pointer
+   use iso_c_binding, only: c_int, c_double, c_double_complex, c_ptr, c_null_ptr, c_associated
 
    implicit none
    private
 
    real(c_double), parameter :: sh_norm = 1.41421356d0
+   integer(c_int), parameter :: SHT_GAUSS = 0
+   integer(c_int), parameter :: SHT_ORTHONORMAL = 0
+   integer(c_int), parameter :: SHT_REAL_NORM = 2048
+   integer(c_int), parameter :: SHT_PHI_CONTIGUOUS = 512
+   integer(c_int), parameter :: SHT_SCALAR_ONLY = 4096
 
    type :: shtns_state
       type(c_ptr) :: cfg = c_null_ptr
@@ -22,6 +27,57 @@ module shtns_backend_mod
    public :: shtns_is_available, shtns_init, shtns_destroy
    public :: shtns_spat2spec, shtns_spec2spat, shtns_configure
 
+   interface
+      function shtns_create_c(lmax, mmax, mres, norm) bind(c, name='shtns_create') result(cfg)
+         use iso_c_binding, only: c_int, c_ptr
+         integer(c_int), value :: lmax, mmax, mres, norm
+         type(c_ptr) :: cfg
+      end function shtns_create_c
+
+      subroutine shtns_set_grid_c(cfg, flags, eps, nlat, nphi) bind(c, name='shtns_set_grid')
+         use iso_c_binding, only: c_int, c_double, c_ptr
+         type(c_ptr), value :: cfg
+         integer(c_int), value :: flags
+         real(c_double), value :: eps
+         integer(c_int), value :: nlat, nphi
+      end subroutine shtns_set_grid_c
+
+      integer(c_int) function shtns_use_threads_c(num_threads) bind(c, name='shtns_use_threads')
+         use iso_c_binding, only: c_int
+         integer(c_int), value :: num_threads
+      end function shtns_use_threads_c
+
+      subroutine shtns_unset_grid_c(cfg) bind(c, name='shtns_unset_grid')
+         use iso_c_binding, only: c_ptr
+         type(c_ptr), value :: cfg
+      end subroutine shtns_unset_grid_c
+
+      subroutine shtns_destroy_c(cfg) bind(c, name='shtns_destroy')
+         use iso_c_binding, only: c_ptr
+         type(c_ptr), value :: cfg
+      end subroutine shtns_destroy_c
+
+      subroutine spat_to_sh_c(cfg, vr, qlm) bind(c, name='spat_to_SH')
+         use iso_c_binding, only: c_ptr, c_double, c_double_complex
+         type(c_ptr), value :: cfg
+         real(c_double), intent(inout) :: vr(*)
+         complex(c_double_complex), intent(out) :: qlm(*)
+      end subroutine spat_to_sh_c
+
+      subroutine sh_to_spat_c(cfg, qlm, vr) bind(c, name='SH_to_spat')
+         use iso_c_binding, only: c_ptr, c_double, c_double_complex
+         type(c_ptr), value :: cfg
+         complex(c_double_complex), intent(in) :: qlm(*)
+         real(c_double), intent(out) :: vr(*)
+      end subroutine sh_to_spat_c
+
+      integer(c_int) function shtns_lmidx_c(cfg, l, m) bind(c, name='shtns_lmidx_fortran')
+         use iso_c_binding, only: c_ptr, c_int
+         type(c_ptr), value :: cfg
+         integer(c_int), value :: l, m
+      end function shtns_lmidx_c
+   end interface
+
 contains
 
    logical function shtns_is_available()
@@ -30,18 +86,15 @@ contains
 
 
    subroutine shtns_init(state, nlon, nlat, ntrunc)
-      include 'external/shtns/shtns.f03'
-
       type(shtns_state), intent(inout) :: state
       integer, intent(in) :: nlon, nlat, ntrunc
-      type(shtns_info), pointer :: info
       integer(c_int) :: norm, layout
       real(c_double) :: eps_polar
 
       call shtns_destroy(state)
 
       norm = SHT_ORTHONORMAL + SHT_REAL_NORM
-      state%cfg = shtns_create(int(ntrunc, c_int), int(ntrunc, c_int), int(1, c_int), norm)
+      state%cfg = shtns_create_c(int(ntrunc, c_int), int(ntrunc, c_int), int(1, c_int), norm)
       if (.not. c_associated(state%cfg)) then
          write(*,*) 'SHTns initialization failed in shtns_create.'
          stop
@@ -49,10 +102,9 @@ contains
 
       layout = SHT_GAUSS + SHT_PHI_CONTIGUOUS + SHT_SCALAR_ONLY
       eps_polar = 1.0d-10
-      call shtns_set_grid(state%cfg, layout, eps_polar, int(nlat, c_int), int(nlon, c_int))
+      call shtns_set_grid_c(state%cfg, layout, eps_polar, int(nlat, c_int), int(nlon, c_int))
 
-      call c_f_pointer(state%cfg, info)
-      state%nlm = info%nlm
+      state%nlm = (ntrunc + 1)*(ntrunc + 2)/2
       state%nlat = nlat
       state%nphi = nlon
       state%ntrunc = ntrunc
@@ -66,8 +118,6 @@ contains
 
 
    subroutine shtns_configure(state, nthreads)
-      include 'external/shtns/shtns.f03'
-
       type(shtns_state), intent(inout) :: state
       integer, intent(in) :: nthreads
       integer(c_int) :: configured
@@ -77,14 +127,12 @@ contains
       endif
 
       if (nthreads > 0) then
-         configured = shtns_use_threads(int(nthreads, c_int))
+         configured = shtns_use_threads_c(int(nthreads, c_int))
       endif
    end subroutine shtns_configure
 
 
    subroutine shtns_destroy(state)
-      include 'external/shtns/shtns.f03'
-
       type(shtns_state), intent(inout) :: state
 
       if (allocated(state%qlm)) then
@@ -95,8 +143,8 @@ contains
       endif
 
       if (c_associated(state%cfg)) then
-         call shtns_unset_grid(state%cfg)
-         call shtns_destroy(state%cfg)
+         call shtns_unset_grid_c(state%cfg)
+         call shtns_destroy_c(state%cfg)
       endif
 
       state%cfg = c_null_ptr
@@ -109,8 +157,6 @@ contains
 
 
    subroutine shtns_spat2spec(z_lat_phi, u_l_m, state)
-      include 'external/shtns/shtns.f03'
-
       type(shtns_state), intent(inout) :: state
       real, intent(in) :: z_lat_phi(:,:)
       complex, intent(out) :: u_l_m(0:,0:)
@@ -131,12 +177,12 @@ contains
          enddo
       enddo
 
-      call spat_to_sh(state%cfg, state%spat_phi_lat, state%qlm)
+      call spat_to_sh_c(state%cfg, state%spat_phi_lat, state%qlm)
 
       u_l_m = (0.0, 0.0)
       do m = 0, state%ntrunc
          do l = m, state%ntrunc
-            lm = shtns_lmidx(state%cfg, int(l, c_int), int(m, c_int))
+            lm = shtns_lmidx_c(state%cfg, int(l, c_int), int(m, c_int))
             u_l_m(l,m) = cmplx(real(state%qlm(lm), c_double), aimag(state%qlm(lm)), kind=kind(u_l_m)) / sh_norm
          enddo
       enddo
@@ -144,8 +190,6 @@ contains
 
 
    subroutine shtns_spec2spat(z_lat_phi, u_l_m, state)
-      include 'external/shtns/shtns.f03'
-
       type(shtns_state), intent(inout) :: state
       real, intent(out) :: z_lat_phi(:,:)
       complex, intent(in) :: u_l_m(0:,0:)
@@ -163,12 +207,12 @@ contains
       state%qlm = (0.0d0, 0.0d0)
       do m = 0, state%ntrunc
          do l = m, state%ntrunc
-            lm = shtns_lmidx(state%cfg, int(l, c_int), int(m, c_int))
+            lm = shtns_lmidx_c(state%cfg, int(l, c_int), int(m, c_int))
             state%qlm(lm) = cmplx(real(u_l_m(l,m), c_double), aimag(u_l_m(l,m)), kind=kind(state%qlm)) * sh_norm
          enddo
       enddo
 
-      call sh_to_spat(state%cfg, state%qlm, state%spat_phi_lat)
+         call sh_to_spat_c(state%cfg, state%qlm, state%spat_phi_lat)
 
       do ip = 1, state%nphi
          do it = 1, state%nlat
