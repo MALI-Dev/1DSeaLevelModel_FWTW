1c50bb0ace56fb19a2011bffc10c16eb72b0b90b
Matthew Hoffman <mhoffman@lanl.gov>
2026-03-11 17:49:33 -0600

    Fix SHTns lmidx ABI usage to avoid init-time segfault

--- shtns_backend_mod.f90
+++ shtns_backend_mod.f90
@@ -18,7 +18,6 @@ module shtns_backend_mod
       integer :: nphi = 0
       integer :: ntrunc = 0
       integer :: nlm = 0
-      integer :: lm_index_base = 0
       logical :: initialized = .false.
       complex(c_double_complex), allocatable :: qlm(:)
       real(c_double), allocatable :: spat_phi_lat(:,:)
@@ -75,7 +74,7 @@ module shtns_backend_mod
       integer(c_int) function shtns_lmidx_c(cfg, l, m) bind(c, name='shtns_lmidx_fortran')
          use iso_c_binding, only: c_ptr, c_int
          type(c_ptr), value :: cfg
-         integer(c_int), value :: l, m
+         integer(c_int), intent(in) :: l, m
       end function shtns_lmidx_c
    end interface
 
@@ -90,8 +89,6 @@ contains
       type(shtns_state), intent(inout) :: state
       integer, intent(in) :: nlon, nlat, ntrunc
       integer(c_int) :: norm, layout
-      integer(c_int) :: lm00_c, lmmax_c
-      integer :: lm00, lmmax
       real(c_double) :: eps_polar
 
       call shtns_destroy(state)
@@ -111,19 +108,6 @@ contains
       state%nlat = nlat
       state%nphi = nlon
       state%ntrunc = ntrunc
-
-      lm00_c = shtns_lmidx_c(state%cfg, int(0, c_int), int(0, c_int))
-      lmmax_c = shtns_lmidx_c(state%cfg, int(ntrunc, c_int), int(ntrunc, c_int))
-      lm00 = int(lm00_c)
-      lmmax = int(lmmax_c)
-      if (lm00 == 0 .and. lmmax == state%nlm - 1) then
-         state%lm_index_base = 0
-      elseif (lm00 == 1 .and. lmmax == state%nlm) then
-         state%lm_index_base = 1
-      else
-         write(*,*) 'Unexpected SHTns lm index range: lm(0,0)=', lm00, ' lm(n,n)=', lmmax, ' nlm=', state%nlm
-         stop
-      endif
 
       allocate(state%qlm(state%nlm))
       allocate(state%spat_phi_lat(state%nphi, state%nlat))
@@ -168,7 +152,6 @@ contains
       state%nphi = 0
       state%ntrunc = 0
       state%nlm = 0
-      state%lm_index_base = 0
       state%initialized = .false.
    end subroutine shtns_destroy
 
@@ -178,7 +161,7 @@ contains
       real, intent(in) :: z_lat_phi(:,:)
       complex, intent(out) :: u_l_m(0:,0:)
       integer :: it, ip, l, m, lm
-      integer(c_int) :: lm_c
+      integer(c_int) :: lm_c, l_c, m_c
 
       if (.not. state%initialized) then
          write(*,*) 'SHTns backend not initialized before shtns_spat2spec.'
@@ -200,8 +183,10 @@ contains
       u_l_m = (0.0, 0.0)
       do m = 0, state%ntrunc
          do l = m, state%ntrunc
-            lm_c = shtns_lmidx_c(state%cfg, int(l, c_int), int(m, c_int))
-            lm = int(lm_c) + 1 - state%lm_index_base
+            l_c = int(l, c_int)
+            m_c = int(m, c_int)
+            lm_c = shtns_lmidx_c(state%cfg, l_c, m_c)
+            lm = int(lm_c)
             if (lm < 1 .or. lm > state%nlm) then
                write(*,*) 'SHTns lm index out of bounds in spat2spec: l,m,lm,nlm=', l, m, lm, state%nlm
                stop
@@ -217,7 +202,7 @@ contains
       real, intent(out) :: z_lat_phi(:,:)
       complex, intent(in) :: u_l_m(0:,0:)
       integer :: it, ip, l, m, lm
-      integer(c_int) :: lm_c
+      integer(c_int) :: lm_c, l_c, m_c
 
       if (.not. state%initialized) then
          write(*,*) 'SHTns backend not initialized before shtns_spec2spat.'
@@ -231,8 +216,10 @@ contains
       state%qlm = (0.0d0, 0.0d0)
       do m = 0, state%ntrunc
          do l = m, state%ntrunc
-            lm_c = shtns_lmidx_c(state%cfg, int(l, c_int), int(m, c_int))
-            lm = int(lm_c) + 1 - state%lm_index_base
+            l_c = int(l, c_int)
+            m_c = int(m, c_int)
+            lm_c = shtns_lmidx_c(state%cfg, l_c, m_c)
+            lm = int(lm_c)
             if (lm < 1 .or. lm > state%nlm) then
                write(*,*) 'SHTns lm index out of bounds in spec2spat: l,m,lm,nlm=', l, m, lm, state%nlm
                stop
